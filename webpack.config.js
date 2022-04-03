a19318dcb203c27dd848b6701bed1afa1e344706
yumeyuniko <daigocafe@msn.com>
2022-04-03 22:34:21 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: {
+    bundle: './src/index.ts',
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js', // [name]はentryで記述した名前（この設定ならbundle）
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: `${__dirname}/dist`,
+    },
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+};
